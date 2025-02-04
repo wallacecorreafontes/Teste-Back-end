be8da2f2fac8b8a26b6b02e055ce3fb09244f8bf
Wallace F <ideiacompany@gmail.com>
2025-02-04 07:20:08 -0400

    Usuários e Autenticação

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Credenciais inválidas'], 401);
+        }
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+# Rotas API`s
+Route::prefix('api')->group(function () {
+
+    # Rotas Públicas
+    Route::post('/login', [AuthController::class, 'login']);
+
+    # Rotas Protegidas (Requer Token JWT)
+    Route::middleware('auth:api')->group(function () {
+
+        # User
+        Route::get('/user', [AuthController::class, 'me']);
+
+    });
 });
